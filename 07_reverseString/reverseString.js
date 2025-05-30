dd1c3c64cb5ee2521cd248f8c06d6ad96459ad24
haimamire <82563520+haimamire@users.noreply.github.com>
2025-05-30 11:51:43 +0900

    Complete exercise 07

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let array = str.split('');
+    return array
+            .reverse()
+            .join('');
 };
 
 // Do not edit below this line
