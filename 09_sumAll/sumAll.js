f430bf0e902eb0c5ee29fc1958bd2638aaef6fd2
haimamire <82563520+haimamire@users.noreply.github.com>
2025-05-30 14:57:44 +0900

    Complete exercise 09

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(minNum, maxNum) {
+    if (!Number.isInteger(minNum) || !Number.isInteger(maxNum)) return 'ERROR';
+    if (minNum < 0 || maxNum < 0) return 'ERROR';
+    // if (isNaN(minNum) || isNaN(maxNum)) return 'ERROR';
+    
+    if (minNum > maxNum) {
+        let temp = minNum;
+        minNum = maxNum;
+        maxNum = temp;
+    }
+    let numbers = [];
+    for (minNum; minNum <= maxNum; minNum++) {
+        numbers.push(minNum);
+    }
+    const sum = numbers.reduce((total, item) => total + item, 0);
+    return sum;
 };
 
 // Do not edit below this line
