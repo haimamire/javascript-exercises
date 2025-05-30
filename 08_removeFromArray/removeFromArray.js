94c9df9634054e9bb00c29ee386f4d851ea91f5c
haimamire <82563520+haimamire@users.noreply.github.com>
2025-05-30 12:17:51 +0900

    Complete exercise 08

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...removed) {
+    let newArray = arr;
+    for (const arg of removed) {
+        newArray = newArray.filter(item => item !== arg);
+    }
+    return newArray;
 };
 
 // Do not edit below this line
