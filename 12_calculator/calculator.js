fb50fe502de8d07d5b65c5d7b5f692f763b3e061
haimamire <82563520+haimamire@users.noreply.github.com>
2025-06-12 07:11:05 +0900

    Complete exercise 12

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((acc, item) => acc + item, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+	return arr.reduce((acc, item) => acc * item, 1)
 };
 
-const power = function() {
-	
+const power = function(number, n) {
+  let result = number;
+  for (let i = 1; i < n; i++) {
+    result *= number;
+  }
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  let result = 1;
+  for (let i = number; i > 0; i--) {
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
