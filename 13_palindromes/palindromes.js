a7badbc1c8f9409a57b18f888c7ce5370eb1afe0
haimamire <82563520+haimamire@users.noreply.github.com>
2025-06-12 07:37:50 +0900

    Complete exercise 13

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
+const palindromes = function(str) {
+    let original = str.toLowerCase().split("");
+    original = original.filter(item => 
+        (item != " "
+        && item != "!"
+        && item != "."
+        && item != ",")
+    );
+    let reversed = original.slice("");
+    reversed.reverse();
 
+    for (let i = 0; i < original.length; i++) {
+        if (original[i] !== reversed[i]) return false;
+    }
+
+    return true;
 };
 
 // Do not edit below this line
