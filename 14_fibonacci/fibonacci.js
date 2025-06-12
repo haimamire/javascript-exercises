b9a94ff057799f3c5c78e1c460fe648489fadacc
haimamire <82563520+haimamire@users.noreply.github.com>
2025-06-12 15:58:49 +0900

    Complete exercise 14

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    if (n < 0) return "OOPS";
+    if (n == 0) return 0;
 
+    let arr = [1, 1];
+    for (let i = 2; i < n; i++) {
+        arr.push(
+            arr[i - 2] + arr[i - 1]
+        );
+    }
+    return arr[n - 1];
 };
 
 // Do not edit below this line
