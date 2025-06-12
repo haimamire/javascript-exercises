da866c67217144839ca72c6fe7b21c41790727ae
haimamire <82563520+haimamire@users.noreply.github.com>
2025-06-12 17:52:12 +0900

    Complete exercise 16

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    people.forEach(
+        element => {
+            if (element.yearOfDeath === undefined) {
+                element.age = new Date().getFullYear() - element.yearOfBirth;                
+            } else {
+                element.age = element.yearOfDeath - element.yearOfBirth;
+            }
+        }
+    );
+    people.sort((a, b) => b.age - a.age);
+    return people[0];
 
+    // return people.reduce(
+    //     (oldest, object) => {
+    //         if (object.age >= oldest.age) {
+    //             oldest = object;
+    //         }
+    //     }, people[0]);
 };
 
 // Do not edit below this line
